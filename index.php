997d5ab1f5e0dab0ba2785afa8099bdee0723f4c
Vaskka <15145051056@163.com>
2018-06-07 14:47:12 +0800

    fix index.php bug

--- index.php
+++ index.php
@@ -2,14 +2,12 @@
 
 use controller\BaseController;
 
-require dirname(__FILE__) . '\\controller\\BaseController.php';
+require dirname(__FILE__) . '/controller/BaseController.php';
 
 date_default_timezone_set('PRC');
 
 $mainController = BaseController::checkGETToGetController($_GET['project']);
 
 $mainController->fromGETToRoute($_GET['status'], $_GET);
-
-
 
 ?>
